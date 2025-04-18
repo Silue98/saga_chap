c07f23aebd564e8c1ba556cf115b11acd69b5a93
Silue98 <94670819+Silue98@users.noreply.github.com>
2025-04-18 11:59:51 +0000

    config navbar

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',  // Si tu utilises toujours ce fichier CSS
+                'resources/js/app.js',    // Fichier JS
+                'resources/sass/app.scss' // Fichier SCSS avec le bon chemin
+            ],
             refresh: [`resources/views/**/*`],
         }),
         tailwindcss(),
