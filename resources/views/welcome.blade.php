22b5983f5035adf5618c8977a9dc915deb9fa071
Silue98 <94670819+Silue98@users.noreply.github.com>
2025-04-22 16:10:06 +0000

    navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,11 +13,25 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Navbar items à gauche -->
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
+      </ul>
+
+      <!-- Formulaire centré -->
+      <div class="d-flex justify-content-center w-100">
+        <form class="d-flex" style="max-width: 500px; width: 100%" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success d-none d-sm-block" type="submit">Rechercher</button> <!-- Masquer le bouton sur les petits écrans -->
+        </form>
+      </div>
+
+      <!-- Navbar items à droite -->
+      <ul class="navbar-nav mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
@@ -33,15 +47,17 @@
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabledn</a>
+          <a class="nav-link disabled" aria-disabled="true">Panier</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
+<div class="card mx-3 my-3" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
 </body>
 </html>
